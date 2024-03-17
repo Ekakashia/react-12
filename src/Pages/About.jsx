a67abe09d4cdd4e345b0ecae1038f1156c52b72f
Ekakashia <127148055+Ekakashia@users.noreply.github.com>
2024-03-17 15:05:04 +0400

    Add files via upload

---
+++ src/Pages/About.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Home from "../Home";
+
+
+
+function About() {
+    const style = {
+
+        backgroundColor: "violet",
+         width: "100%",
+        
+           
+        }
+
+   
+    return(
+
+        <React.Fragment>
+           
+        <Home></Home>
+        
+        <p style={style}>About Us</p>
+
+      </React.Fragment>
+  );
+}
+
+export default About;
